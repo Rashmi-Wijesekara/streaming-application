779c2d3372fabccac00a05927e032962e21a63e6
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2023-03-22 11:35:08 +0530

    setup rtmp server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,26 +1,33 @@
-import React, {useEffect} from 'react'
-import {connect, useSelector} from 'react-redux'
-import {fetchStream} from '../../actions'
-import {useParams} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { connect, useSelector } from "react-redux";
+import { fetchStream } from "../../actions";
+import { useParams } from "react-router-dom";
+import flv from "flv.js";
 
 const StreamShow = (props) => {
-	const {id} = useParams()
+	const videoRef = React.createRef();
+
+	const { id } = useParams();
 
 	const stream = useSelector((state) => {
 		return state.streams[id];
 	});
 
-
 	useEffect(() => {
-		props.fetchStream(id)
-	}, [id, props])
-	
-	return stream && (
-		<div>
-			<div className="ui header">{stream.title}</div>
-			<div className="ui content">{stream.description}</div>
-		</div>
-	)
-}
+		props.fetchStream(id);
+	}, [id, props]);
 
-export default connect(null, {fetchStream})(StreamShow)
+	return (
+		stream && (
+			<div>
+				<video ref={videoRef} style={{width: '100%'}} controls />
+				<div className="ui header">{stream.title}</div>
+				<div className="ui content">
+					{stream.description}
+				</div>
+			</div>
+		)
+	);
+};
+
+export default connect(null, { fetchStream })(StreamShow);
